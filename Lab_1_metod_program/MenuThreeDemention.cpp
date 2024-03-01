eebd086857fd70f0884c93736c7dd64e5fedceb6
phazze- <78924742+Phazze@users.noreply.github.com>
2024-03-01 12:22:14 +0300

    Update MenuThreeDemention.cpp

--- Lab_1_metod_program/MenuThreeDemention.cpp
+++ Lab_1_metod_program/MenuThreeDemention.cpp
@@ -9,20 +9,20 @@ void MenuThreeDemention()
 
 	while (result)
 	{
-		cout << "Введите 1, чтобы выполнить задание №3\n";
-		cout << "Введите 0, чтобы вернуться в главное меню\n";
+		cout << "Press 1 to complete task #3\n";
+		cout << "Press 0 to return to main menu\n";
 
 		cin >> choise;
 
 		if (choise == '0')
 		{
-			cout << "Вы уверены, что хотите вернуться в главное меню? (Y/N)" << endl;
+			cout << "Are you sure you want to return to the main menu? (Y/N)" << endl;
 
 			cin >> confirmation;
 
 			if (confirmation == 'Y' || confirmation == 'y')
 			{
-				cout << "Возвращаю Вас в главное меню!\n";
+				cout << "I return you to the main menu!\n";
 				Interface();
 				result = false;
 				break;
@@ -30,24 +30,24 @@ void MenuThreeDemention()
 			if (confirmation == 'N' || confirmation == 'n')
 			{
 				result = true;
-				cout << "Возвращаю Вас в меню задания №3." << endl;
+				cout << "I return you to the task menu 3." << endl;
 			}
 			else
 			{
 				result = true;
-				cout << "Не понял вашу команду возвращаю Вас в меню задания №3!" << endl;
+				cout << "I didnвЂ™t understand your command, IвЂ™m returning you to the task menu 3!" << endl;
 			}
 		}
 
 		switch (choise)
 		{
 		case '1':
-			cout << "Выполняется задание №3\n";
+			cout << "Task 3 in progress!\n";
 			//ResultOneDemention();
 			result = false;
 			break;
 		default:
-			cout << "Введенное значение не соответствует номеру задания, введите значение еще 1 раз\n";
+			cout << "I didnвЂ™t understand your command, IвЂ™m returning you to the task menu 3!\n";
 			break;
 		}
 	}
